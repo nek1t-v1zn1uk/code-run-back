416241480d529ce5c438b2d4568522c981b8527b
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2026-05-12 19:38:11 +0300

    feat: add simple solution evaluation with script checkers

--- src/main/kotlin/com/example/coderun/domain/solutions/service/SolutionEvaluationService.kt
+++ src/main/kotlin/com/example/coderun/domain/solutions/service/SolutionEvaluationService.kt
@@ -5,13 +5,12 @@ import com.example.coderun.domain.problems.entity.EvaluationType
 import com.example.coderun.domain.solutions.entity.Solution
 import com.example.coderun.domain.solutions.entity.SolutionStatus
 import com.example.coderun.domain.solutions.repository.SolutionRepository
+import com.example.coderun.domain.tests.repository.ScriptCheckerRepository
 import org.springframework.stereotype.Service
 import java.time.Instant
 import java.util.LinkedList
 import java.util.Queue
-import kotlin.math.round
 import kotlin.math.roundToInt
-import kotlin.math.roundToLong
 
 @Service
 class SolutionEvaluationService(
@@ -35,44 +34,58 @@ class SolutionEvaluationService(
         var index = 0
         for(test in tests) {
             index++
-            val result = isolateService.executeCode(
+            val solutionResult = isolateService.executeCode(
                 solution.code!!,
                 solution.language.language,
                 test.inputData,
                 problem.executionTimeLimitMs / 1000F,
                 problem.executionMemoryLimitKb
             )
-            val evaluationType = test.overrideEvaluationType ?: problem.defaultEvaluationType!!
-            if(evaluationType == EvaluationType.EXACT_MATCH) {
-                if(result.status == "OK"){
-                    if(!test.expectedOutput.equals(result.stdout.trim())) {
+
+            if(solutionResult.status == "OK"){
+                val evaluationType = test.overrideEvaluationType ?: problem.defaultEvaluationType!!
+                if(evaluationType == EvaluationType.EXACT_MATCH) {
+                    if(!test.expectedOutput.equals(solutionResult.stdout.trim())) {
                         solution.status = SolutionStatus.TEST_FAILED
                         solution.testCaseReached = index
                         break
                     }
-                } else {
-                    println(result)
-                    solution.status = when(result.status) {
-                        "TO" -> {
-                            solution.executionTimeMs = (result.time*1000).roundToInt()
-                            SolutionStatus.TIME_LIMIT_EXCEEDED
-                        }
-                        "RE" -> SolutionStatus.RUNTIME_ERROR
-                        "SG" -> {
-                            when (result.exitSignal) {
-                                9 -> {
-                                    solution.executionMemoryKb = result.memory.toInt()
-                                    SolutionStatus.MEMORY_LIMIT_EXCEEDED
-                                }
-                                25 -> SolutionStatus.OUTPUT_LIMIT_EXCEEDED // doesnt work for now
-                                else -> SolutionStatus.INTERNAL_ERROR
-                            }
-                        }
-                        else -> SolutionStatus.INTERNAL_ERROR
+                } else if (evaluationType == EvaluationType.SCRIPT_CHECK) {
+                    val scriptChecker = test.overrideScriptChecker ?: problem.defaultScriptChecker!!
+                    val checkerResult = isolateService.executeCode(
+                        scriptChecker.code,
+                        scriptChecker.language.language,
+                        solutionResult.stdout.trim() + "\n" + test.inputData + "\n" + test.expectedOutput,
+                        10F,
+                        1024*1024
+                    )
+                    if(checkerResult.status != "OK" || checkerResult.stdout == "False") {
+                        solution.status = SolutionStatus.TEST_FAILED
+                        solution.testCaseReached = index
+                        break
                     }
-                    solution.testCaseReached = index
-                    break
                 }
+            } else {
+                solution.status = when(solutionResult.status) {
+                    "TO" -> {
+                        solution.executionTimeMs = (solutionResult.time*1000).roundToInt()
+                        SolutionStatus.TIME_LIMIT_EXCEEDED
+                    }
+                    "RE" -> SolutionStatus.RUNTIME_ERROR
+                    "SG" -> {
+                        when (solutionResult.exitSignal) {
+                            9 -> {
+                                solution.executionMemoryKb = solutionResult.memory.toInt()
+                                SolutionStatus.MEMORY_LIMIT_EXCEEDED
+                            }
+                            25 -> SolutionStatus.OUTPUT_LIMIT_EXCEEDED // doesnt work for now
+                            else -> SolutionStatus.INTERNAL_ERROR
+                        }
+                    }
+                    else -> SolutionStatus.INTERNAL_ERROR
+                }
+                solution.testCaseReached = index
+                break
             }
         }
         if(index == tests.size)
