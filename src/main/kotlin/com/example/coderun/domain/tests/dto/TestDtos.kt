b57df5ba78a42c7afdda73c7bddf94b6953e5d38
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2026-05-13 17:59:25 +0300

    feat: add test get endpoint, list of tests for problem

--- src/main/kotlin/com/example/coderun/domain/tests/dto/TestDtos.kt
+++ src/main/kotlin/com/example/coderun/domain/tests/dto/TestDtos.kt
@@ -1,7 +1,28 @@
 package com.example.coderun.domain.tests.dto
 
 import com.example.coderun.domain.problems.entity.EvaluationType
+import com.example.coderun.domain.problems.entity.Problem
+import com.example.coderun.domain.tests.entity.ScriptChecker
+import jakarta.persistence.Column
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.validation.constraints.NotNull
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
+import java.time.Instant
+
+data class TestDto(
+    var id: Int,
+    var problemId: Int,
+    var ordinal: Int,
+    var isExample: Boolean,
+    var inputData: String,
+    var expectedOutput: String? = null,
+    var overrideEvaluationType: EvaluationType? = null,
+    var overrideScriptCheckerId: Int? = null,
+    var createdAt: Instant
+)
 
 data class UpdateTestListRequest(
     val newTests: List<NewTestRequest> = emptyList(),
@@ -16,7 +37,8 @@ data class NewTestRequest(
     @field:NotNull
     val inputData: String? = null,
     val expectedOutput: String? = null,
-    var overrideEvaluationType: EvaluationType? = null,
+    val overrideEvaluationType: EvaluationType? = null,
+    val overrideScriptCheckerId: Int? = null,
 )
 data class UpdateTestRequest(
     @field:NotNull
@@ -26,6 +48,7 @@ data class UpdateTestRequest(
     val inputData: String? = null,
     val expectedOutput: String? = null,
     var overrideEvaluationType: EvaluationType? = null,
+    val overrideScriptCheckerId: Int? = null,
 )
 data class DeleteTestRequest(
     @field:NotNull
